48ba1c8f9db66799bce47648a9c1dac4e4187422
AA274 <chilinghan2007@gmail.com>
2025-10-23 17:59:42 -0700

    section 4 sim code

--- autonomy_repo/scripts/heading_controller.py
+++ autonomy_repo/scripts/heading_controller.py
@@ -8,7 +8,11 @@ from asl_tb3_msgs.msg import TurtleBotControl, TurtleBotState
 class HeadingController(BaseHeadingController):
     def __init__(self):
         super().__init__()
-        self.kp = 10.0
+        self.declare_parameter("kp", 2.0)
+
+    @property
+    def kp(self) -> float:
+        return self.get_parameter("kp").value
 
     def compute_control_with_goal(self, currState: TurtleBotState, goalState: TurtleBotState) -> TurtleBotControl:
         error = goalState.theta - currState.theta
